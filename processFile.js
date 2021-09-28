7505fd5633f0e32936890c81487a2733a2fd0318
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2021-09-27 20:06:13 -0400

    adding additional comments

--- processFile.js
+++ processFile.js
@@ -20,6 +20,8 @@ let processing = false;
 
 cron.schedule('* * * * *', async () => {
     console.log('Checking queue for new files...');
+    // Only allow one instance of the worker to run at a time in case a file has a processing time longer than the cron job interval (1 min)
+    // This can be triggered by adding a file to the `processing` directory with a value above 60.00
     if (processing === true) {
         console.log('Already processing a file!');
         return;
@@ -35,15 +37,21 @@ cron.schedule('* * * * *', async () => {
     if (fileToProcess) {
         console.log(`Starting processing...`);
         processing = true;
+        
         const { filename, processingTime } = fileToProcess;
+        
+        // Index of the currently processing file
         const fileIndex = stateData.findIndex(file => file.filename === filename);
+        
+        // Move the file into `processing` status
         const processingFile = await updateIndex(statePath, fileIndex, { ...fileToProcess, status: 'processing', worker });
         console.log(`Currently processing ${chalk.blueBright(filename)} | Estimated time: ${chalk.yellow.bold(parseInt(processingTime))}s`);
         
         setTimeout(async () => {
-            await updateIndex(statePath, fileIndex, { ...processingFile, status: 'processed' });
+            // Update the status of the file to complete
+            await updateIndex(statePath, fileIndex, { ...processingFile, status: 'complete' });
             console.log(`Finished processing ${chalk.blueBright(filename)} after ${chalk.yellow.bold(parseInt(processingTime))}s`);
-            processing = false;    
+            processing = false;
         }, processingTime * 1000);
     }
 });
